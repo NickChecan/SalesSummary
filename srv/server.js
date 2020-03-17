cd19e05e0af56defc5bb76400e4a6ee118e54fa6
NickChecan <nicholas.checan@gmail.com>
2020-03-17 19:52:21 +0000

    API communication using Express

--- srv/server.js
+++ srv/server.js
@@ -1,12 +1,37 @@
 /*eslint no-console: 0*/
 "use strict";
 
-var http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+var app = express();
+
+// Define which port will run the application
 var port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World\n");
-}).listen(port);
+// Prepare app to automatically understand content-type json 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-console.log("Server listening on port %d", port);
+// Get request example
+app.get('/', (req, res) => res.send("Hello World!"));
+
+// Request Example: {url}/read/1?name=Nicholas
+app.get('/read/:num', (req, res) => {
+    let name = req.query.name
+    console.log(name)
+    let num = req.params.num
+    if (num > 5) res.send("Greater than 5!")
+    else res.send("Less than 5!")
+});
+
+// Post request example
+app.post('/save', (req, res) => {
+    let name = req.body.name
+    let number = req.body.number
+    console.log("Received name: " + name + ". Received number: " + number)
+    res.send("Ok!")
+    res.status(204)
+})
+
+// Enable the server to be accessed
+app.listen(port, () => console.log(`Listening on port ${port}!`));
